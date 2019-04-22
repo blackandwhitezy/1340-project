15c4163b9c7df90eb52e513316b5fbddb976556e
hht-hannah <47147702+hht-hannah@users.noreply.github.com>
2019-04-22 12:01:08 +0800

    Update Program.cpp

--- Program.cpp
+++ Program.cpp
@@ -20,7 +20,7 @@ int readlist(int good[]){
   
   string line,name,profit;int i=0;
   ifstream fin("goods.txt");
-  isringstream line_in(line);
+  istringstream line_in(line);
   while (getline(fin,line)){
     line_in>>name;
     line_in>>profit;
@@ -40,22 +40,6 @@ void generate_random(customer line[],int cus_pos, int good[], int good_no){
   }
 }
 
-//return maxprofit
-int arrange_line(int cus_no,int cus_chosen_id[],customer *p){
-  int test,*pack=new int[cus_no+1][TIME+1];
-    for (int i=0;i<cus_no+1;i++)
-      for (int j=0;j<TIME+1;j++)
-        pack[i][j]=0;
-  for (int i=1;i<cus_no+1;i++){
-    for (int j=1;j<TIME+1;j++){
-      test=(p[i].item>j?pack[i][j-1]:pack[i-1][j-p[i].item]+p[i].profit);
-      test=isBigger(test,pack[i][j-1],pack[i-1][j]);
-    }
-  }
-  delete [] pack;
-  return pack[n-1][TIME-1];  
-}
-
 int isBigger(int a,int b,int c){
   int temp;
   if (a > b){
@@ -70,12 +54,31 @@ int isBigger(int a,int b,int c){
   return temp;
 }
 
+//return maxprofit
+int arrange_line(int cus_no,int cus_chosen_id[],customer *p){
+  int test,**pack=new int*[cus_no+1];
+  for (int i=0;i<cus_no+1;i++){
+    pack[i] = new int [TIME+1];
+  }
+    for (int i=0;i<cus_no+1;i++)
+      for (int j=0;j<TIME+1;j++)
+        pack[i][j]=0;
+  for (int i=1;i<cus_no+1;i++){
+    for (int j=1;j<TIME+1;j++){
+      test=(p[i].item_no>j?pack[i][j-1]:pack[i-1][j-p[i].item_no]+p[i].profit);
+      test=isBigger(test,pack[i][j-1],pack[i-1][j]);
+    }
+  }
+  delete [] pack;
+  return pack[cus_no-1][TIME-1];  
+}
+
 void print_line(){}
 
 int main()
 {
-  int good[GOOD_MAX],line[CUSTOMER_MAX];
-  good_no = readlist(good);
+  int good[GOOD_MAX];
+  int good_no = readlist(good);
   int n;
   cout << "How many customers in the line?" <<endl;
   cin >> n;
