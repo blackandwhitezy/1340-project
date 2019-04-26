5860ab40f7d5d7b0e22484c6f0cf96966b35947f
hht-hannah <47147702+hht-hannah@users.noreply.github.com>
2019-04-26 22:05:28 +0800

    Create readlist.cpp

---
+++ readlist.cpp
@@ -0,0 +1,14 @@
+//creat the good array based on good list. Return the total number of goods available.
+int readlist(int good[],string goodname[]){
+  string line,name,profit;int i=0;
+  ifstream fin("goods.txt");
+  while (getline(fin,line)){
+    istringstream line_in(line);
+    line_in>>goodname[i];
+    line_in>>profit;
+    good[i]=atoi(profit.c_str());
+    i++;
+  }
+  fin.close();
+  return i;
+}
