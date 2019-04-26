9608c9bac20efa892058395933d31c509a9d3efe
hht-hannah <47147702+hht-hannah@users.noreply.github.com>
2019-04-26 22:09:23 +0800

    Create trace_line.cpp

---
+++ trace_line.cpp
@@ -0,0 +1,14 @@
+//trace the customer chosen in the cashier
+int trace_line(int trace[],int i, int j,customer *&p,int **&pack){
+  if (i == 0){
+    return 0;
+  }
+  if (pack[i][j] == pack[i-1][j]){
+    trace_line (trace,i-1,j,p,pack);
+  }
+  else if (pack[i][j] == pack[i-1][j-p[i-1].item_no]+p[i-1].profit && pack[i][j]!=0){
+    trace[i-1] = i;
+    p[i-1].profit=0;
+    trace_line(trace,i-1,j-p[i-1].item_no,p,pack);
+  }
+}
