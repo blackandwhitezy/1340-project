d330843c7d3f509162075789f5f5adf35f981d65
hht-hannah <47147702+hht-hannah@users.noreply.github.com>
2019-04-26 22:42:16 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+const int GOOD_MAX=100;
+const int TIME=90;
+const int COST_PER_LINE=250;
+const int CUSTOMER_MAX=500;
+
+struct customer{
+  double profit;
+  int item_no;
+};
+
+int main()
+{
+  int good[GOOD_MAX];
+  string goodname[GOOD_MAX];
+  int good_no = readlist(good,goodname);
+  int cus_no=0,profit=0,cashier_counter=0;
+  int **pack;
+  cout << "How many customers in the line?" <<endl;
+  cin >> cus_no;
+  customer *line = new customer[cus_no];
+  for (int i=0;i<cus_no;i++){
+    line[i].profit=0;
+    line[i].item_no=0;
+  }
+  srand(time(0));
+  for (int i=0;i<cus_no;i++){
+    generate_random(line,i,good,good_no,goodname);
+  }
+  profit=arrange_line(cus_no,line,pack);
+  while (profit>COST_PER_LINE){
+    int trace[100]={0};
+    cashier_counter+=1;
+    trace_line(trace,cus_no,TIME,line,pack);
+    print_line(trace,cus_no,profit,cashier_counter);
+    profit=arrange_line(cus_no,line,pack);
+  }
+  delete [] pack;
+  delete [] line;
+}
