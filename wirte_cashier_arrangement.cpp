53919dabe858705e94718d573abfc1d940e5f3cb
blackandwhitezy <43725518+blackandwhitezy@users.noreply.github.com>
2019-04-27 00:23:41 +0800

    Update and rename print_line.cpp to wirte_cashier_arrangement.cpp

--- wirte_cashier_arrangement.cpp
+++ wirte_cashier_arrangement.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-void print_line(int customer_number[],int number, int profit, int cashier_counter){
-  ofstream fout("Line_Customer.txt",ios::app);
+void wirte_cashier_arrangement(int customer_number[],int number, int profit, int cashier_counter){
+  ofstream fout("Cashier_Arrangemet.txt",ios::app);
   fout << "Cashier #" << cashier_counter<<endl;
   for(int i=0; i<number;i++){
     if (customer_number[i] !=0)
