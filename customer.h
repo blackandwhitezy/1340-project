2899c50353e5c2984b2f89498a8725de835df368
blackandwhitezy <43725518+blackandwhitezy@users.noreply.github.com>
2019-04-27 01:31:50 +0800

    Update customer.h

--- customer.h
+++ customer.h
@@ -1,10 +1,9 @@
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
 
-const int GOOD_MAX=100;
+
 const int TIME=90;
-const int COST_PER_LINE=250;
-const int CUSTOMER_MAX=500;
+
 
 struct customer{
   double profit;
