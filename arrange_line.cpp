e5d508e321dab2ee1a0226c91537c1c62670a471
hht-hannah <47147702+hht-hannah@users.noreply.github.com>
2019-04-26 22:08:32 +0800

    Create arrange_line.cpp

---
+++ arrange_line.cpp
@@ -0,0 +1,19 @@
+//return maxprofit
+int arrange_line(int cus_no,customer *&p,int **&pack){
+  int test=0;
+  pack=new int*[cus_no+1];
+  for (int i=0;i<cus_no+1;i++){
+    pack[i] = new int [TIME+1];
+  }
+  for (int i=0;i<cus_no+1;i++)
+    for (int j=0;j<TIME+1;j++)
+      pack[i][j]=0;
+  for (int i=1;i<cus_no+1;i++){
+    for (int j=1;j<TIME+1;j++){
+      test=(p[i-1].item_no>j?pack[i][j-1]:pack[i-1][j-p[i-1].item_no]+p[i-1].profit);
+      test=isBigger(test,pack[i][j-1],pack[i-1][j]);
+      pack[i][j]=test;
+    }
+  }
+  return pack[cus_no][TIME];
+}
