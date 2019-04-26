b8eba66549f1861e885ee034716e0d4204a4954d
hht-hannah <47147702+hht-hannah@users.noreply.github.com>
2019-04-26 22:10:17 +0800

    Create print_line.cpp

---
+++ print_line.cpp
@@ -0,0 +1,10 @@
+void print_line(int customer_number[],int number, int profit, int cashier_counter){
+  ofstream fout("Line_Customer.txt",ios::app);
+  fout << "Cashier #" << cashier_counter<<endl;
+  for(int i=0; i<number;i++){
+    if (customer_number[i] !=0)
+      fout << "Customer "<< left <<setw(3) << customer_number[i] << "being chosen "<< endl;
+  }
+  fout << "Total profit for this cashier: $" << profit <<endl;
+  fout.close();
+}
