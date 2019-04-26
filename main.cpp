e62758f8a6493e96f3fb9652d7638dc377958212
blackandwhitezy <43725518+blackandwhitezy@users.noreply.github.com>
2019-04-27 00:49:32 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,7 @@
 #include <ctime>
 using namespace std;
 
-const int GOOD_MAX=100;
-const int TIME=90;
-const int COST_PER_LINE=250;
-const int CUSTOMER_MAX=500;
 
-struct customer{
-  double profit;
-  int item_no;
-};
 
 int main()
 {
