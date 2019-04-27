fb9eebf4865ab23f492468f6009cef7b695774b7
blackandwhitezy <43725518+blackandwhitezy@users.noreply.github.com>
2019-04-27 09:47:07 +0800

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 #include<iostream>
 #include"read_goods_file.h"
-#include"generate_random_customer.h"
+#include"generate_random_customers.h"
 #include"arrange_line.h"
 #include"trace_line.h"
 #include"write_cashier_arrangment.h"
