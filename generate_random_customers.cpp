7a9c6d94e78083da17ec1eb1f0348b0a3a7496c6
blackandwhitezy <43725518+blackandwhitezy@users.noreply.github.com>
2019-04-27 10:59:16 +0800

    Update generate_random_customers.cpp

--- generate_random_customers.cpp
+++ generate_random_customers.cpp
@@ -8,7 +8,7 @@ void generate_random_customers(customer line[],int cus_pos, int good[], int good
   ofstream fout("Raw_Customer.txt",ios::app);
   fout << "Customer "<<left<< setw(3)<<cus_pos+1 <<"bought ";
   for (int i=0;i<good_no;i++){
-    n = (rand()%6)*(rand()%2);
+    n = (rand()%6)*(rand()%2)*(rand()%2);
     fout <<setw(2) <<n << goodname[i]<< ", ";
     line[cus_pos].profit += good[i] * n;
     line[cus_pos].item_no += n;
