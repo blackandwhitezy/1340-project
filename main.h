c350f16005f3a1eadc5702a93543759a996b6fba
blackandwhitezy <43725518+blackandwhitezy@users.noreply.github.com>
2019-04-27 09:30:35 +0800

    Update main.h

--- main.h
+++ main.h
@@ -1,9 +1,9 @@
 #include<iostream>
-#include<read_goods_file.h>
-#include<generate_random_customer.h>
-#include<arrange_line.h>
-#include<trace_line.h>
-#include<write_cashier_arrangment.h>
+#include"read_goods_file.h"
+#include"generate_random_customer.h"
+#include"arrange_line.h"
+#include"trace_line.h"
+#include"write_cashier_arrangment.h"
 
 #define COST_PER_LINE 250
 #define CUSTOMER_MAX 500
