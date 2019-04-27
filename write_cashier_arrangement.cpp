6a7c7a6a5f54a454987c5b3ca6a2fed7014cf9f6
blackandwhitezy <43725518+blackandwhitezy@users.noreply.github.com>
2019-04-27 10:33:02 +0800

    Update write_cashier_arrangement.cpp

--- write_cashier_arrangement.cpp
+++ write_cashier_arrangement.cpp
@@ -2,7 +2,7 @@
 #include"write_cashier_arrangement.h"
 using namespace std;
 
-void wirte_cashier_arrangement(int customer_number[],int number, int profit, int cashier_counter){
+void write_cashier_arrangement(int customer_number[],int number, int profit, int cashier_counter){
   ofstream fout("Cashier_Arrangemet.txt",ios::app);
   fout << "Cashier #" << cashier_counter<<endl;
   for(int i=0; i<number;i++){
