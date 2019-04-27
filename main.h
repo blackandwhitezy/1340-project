7599ec0c803fbf3064322db565969decd71185c3
blackandwhitezy <43725518+blackandwhitezy@users.noreply.github.com>
2019-04-27 09:48:39 +0800

    Update main.h

--- main.h
+++ main.h
@@ -3,7 +3,7 @@
 #include"generate_random_customers.h"
 #include"arrange_line.h"
 #include"trace_line.h"
-#include"write_cashier_arrangment.h"
+#include"write_cashier_arrangement.h"
 
 #define COST_PER_LINE 250
 #define CUSTOMER_MAX 500
